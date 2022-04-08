91338ad85c436cc18052a77e1e52443524e8dfa2
Eugene Stolbin <estolbin@gmail.com>
2022-04-08 21:16:04 +0300

    ClassWork DONE

--- Police/main.cpp
+++ Police/main.cpp
@@ -1,194 +1,212 @@
 ﻿#include<iostream>
-#include<map>
-#include<string>
 #include<fstream>
+#include<string>
 #include<list>
+#include<conio.h>
 #include<Windows.h>
+#include<map>
 
-using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
 
-const std::string FILENAME = "base.txt";
+#define tab "\t"
 
-typedef std::map<std::string, std::list<std::string>> base_list;
+const std::map<int, std::string> CRIMES =
+{
+	std::pair<int, std::string>(1, "Проезд на красный."),
+	std::pair<int, std::string>(2, "Превышение скорости."),
+	std::pair<int, std::string>(3, "Езда по встречной полосе."),
+	std::pair<int, std::string>(4, "Дрифт на перекрестке."),
+	std::pair<int, std::string>(5, "Парковка в неположенном месте."),
+	std::pair<int, std::string>(6, "Оскорбление офицера."),
+	std::pair<int, std::string>(7, "Вождение в нетрезвом состоянии."),
+	std::pair<int, std::string>(8, "Вождение без документов."),
+};
 
-void add_to_tree(base_list& GaiMap, std::string numbera, std::string hhhh);
-void list_all(base_list& GaiMap);
-void read_from_file(base_list& GaiMap);
-void add_record(base_list& GaiMap);
-void save_file(base_list& GaiMap);
-void list_for_number(base_list& GaiMap, std::string number);
-void partial_list(base_list& GaiMap);
-void main_menu();
+class Crime
+{
+	int id;
+	std::string place;
+public:
+	int get_id() const { return id; }
+	const std::string get_place() const { return place; }
+	Crime(int id, std::string place):id(id), place(place) {}
+	~Crime(){}
+};
+
+std::ostream& operator<<(std::ostream& os, const Crime& obj)
+{
+	return os << CRIMES.at(obj.get_id()) << " - " << obj.get_place();
+}
+
+std::ofstream& operator<<(std::ofstream& ofs, const Crime& obj)
+{
+	ofs << obj.get_id() << obj.get_place();
+	return ofs;
+}
+
+void print(const std::map<std::string, std::list<Crime>>& base);
+void save(const std::map<std::string, std::list<Crime>>& base, const std::string& filename);
+void load(std::map<std::string, std::list<Crime>>& base, const std::string filename);;
+int check_crime();
+std::string input_place();
+std::string input_plate();
+void menu(std::map<std::string, std::list<Crime>>& base, const std::string& filename);
+
+//#define ADDITION_TO_BASE
 
 void main()
 {
-    //setlocale(LC_ALL, "");
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    base_list GaiMap;
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	std::map<std::string, std::list<Crime>> base;
+	load(base, "base.txt");
+	print(base);
 
-    read_from_file(GaiMap);
-    int choice;
-    std::string car_num;
-    do
-    {
-        main_menu();
-        cin >> choice;
+#ifdef ADDITION_TO_BASE
+	std::string licence_plate;	
+	int id;
+	std::string place;
+	cout << "Введите номер автомобиля: "; cin >> licence_plate;
+	/*cout << "Выберите правонарушение:  "; cin >> id;
+	id = check_crime();
+	cout << "Введите место происшествия:";
+	cin.ignore();
+	std::getline(std::cin, place);*/
+	//base[licence_plate].push_back(Crime(id, place));
 
-        switch (choice)
-        {
-        case 1:
-            list_all(GaiMap);
-            break;
-        case 2:
-            add_record(GaiMap);
-            break;
-        case 3:
-            partial_list(GaiMap);
-            break;
-        case 4:
-            cin.ignore();
-            cout << "Номер для поиска: ";
-            getline(cin, car_num);
-            list_for_number(GaiMap, car_num);
-            break;
-        case 5:
-            cout << "Bye!";
-            break;
-        default:
-            main_menu();
-            cin >> choice;
-        }
+	base[licence_plate].push_back(Crime(check_crime(), input_place()));
+	print(base);
+#endif // ADDITION_TO_BASE
 
-    } while (choice != 5);
-}
-void add_to_tree(base_list& GaiMap, std::string numbera, std::string hhhh)
-{
-    auto it = GaiMap.find(numbera);
-    if (it == GaiMap.end())
-    {
-        list<std::string> l;
-        l.push_back(hhhh);
-        GaiMap.insert(make_pair(numbera, l));
-    }
-    else
-        it->second.push_back(hhhh);
-}
-void list_all(base_list& GaiMap)
-{
-    cout << "Номер" << "\t\t" << "Нарушения";
-    cout << "\n----------------------------\n";
-    for (auto p : GaiMap)
-    {
-        cout << p.first;
-        list<std::string> get = p.second;
-        for (auto it = get.begin(); it != get.end(); ++it)
-            cout << "\t\t" << (*it) << '\n';
-        cout << endl;
-    }
-}
-void read_from_file(base_list& GaiMap)
-{
-    ifstream fin(FILENAME);
-    int n = 0;
-    if (fin.is_open())
-    {
-        std::string buffer;
-        while (!fin.eof())
-        {
-            std::getline(fin, buffer);
-            if (buffer.size())
-            {
-                std::string number = buffer.substr(0, buffer.find(">>"));
-                std::string hhh = buffer.substr(buffer.find(">>") + 2);
+	menu(base, "base.txt");
 
-                add_to_tree(GaiMap, number, hhh);
-            }
-        }
-        fin.close();
-    }
-}
-void add_record(base_list& GaiMap)
-{
-    std::string number;
-    cin.ignore();
-    cout << "Введите номер машины: ";
-    getline(cin, number);
-
-    std::string hhhh;
-    cin.ignore();
-    cout << "Введите нарушение: ";
-    getline(cin, hhhh);
-
-    add_to_tree(GaiMap, number, hhhh);
-    save_file(GaiMap);
-}
-void save_file(base_list& GaiMap)
-{
-    ofstream fout;
-    fout.open(FILENAME);
-    if (!fout) return;
-
-    for (auto p : GaiMap)
-    {
-        list<std::string> get = p.second;
-        for (auto it = get.begin(); it != get.end(); ++it)
-            fout << p.first << ">>" << (*it) << '\n';
-        fout << endl;
-    }
-    fout.close();
-}
-void main_menu()
-{
-    cout << "\nБаза данных\n";
-    cout << "-----------\n";
-    cout << "Выберите один из пунктов\n";
-    cout << "1 - Вывести весь список\n";
-    cout << "2 - Добавить запись\n";
-    cout << "3 - Частичный вывод (с..по)\n";
-    cout << "4 - Вывод по номеру\n";
-    cout << "5 - Выход\n";
-    cout << "Выбранное значение: ";
-}
-void list_for_number(base_list& GaiMap, std::string number)
-{
-    auto it = GaiMap.find(number);
-    if (it == GaiMap.end())
-    {
-        cout << "Номер \"чист\". Отличный водитель :)" << endl;
-    }
-    else
-    {
-        list<std::string> get = it->second;
-        cout << "Список нарушений по номеру " << number << endl << endl;
-        int count = 0;
-        for (auto i = get.begin(); i != get.end(); ++i)
-            cout << (++count) << ". " << *i << endl;
-    }
+	print(base);
+	save(base, "base.txt");
 }
 
-void partial_list(base_list& GaiMap)
+void print(const std::map<std::string, std::list<Crime>>& base)
 {
-    std::string str_itlow, str_itupper;
-    base_list::iterator itlow, itupper;
+	system("CLS");	
+	for (std::map<std::string, std::list<Crime>>::const_iterator it = base.begin(); it != base.end(); ++it)
+	{
+		cout << it->first << ":\n";
+		for (std::list<Crime>::const_iterator jt = it->second.begin(); jt != it->second.end(); ++jt)
+		{
+			cout << *jt << ";\n";
+		}
+		cout << "\n-----------------------------------------------\n";
+	}
+	system("PAUSE");
+}
+void save(const std::map<std::string, std::list<Crime>>& base, const std::string& filename)
+{
+	std::ofstream fout(filename);
+	for (std::map<std::string, std::list<Crime>>::const_iterator it = base.begin(); it != base.end(); ++it)
+	{
+		fout << it->first << ":";
+		for (std::list<Crime>::const_iterator jt = it->second.begin(); jt != it->second.end(); ++jt)
+		{
+			fout << *jt << ",";
+		}
+		fout.seekp(-1, std::ios::cur);
+		fout << ";\n";
+	}
+	fout.close();
+	std::string command = "notepad ";
+	command += filename;
+	system(command.c_str());
+}
+void load(std::map<std::string, std::list<Crime>>& base, const std::string filename)
+{
+	std::ifstream fin(filename);
+	if (fin.is_open())
+	{
+		//TODO: read file
+		while (!fin.eof())
+		{
+			std::string licence_plate;
+			std::string all_crimes;
+			std::getline(fin, licence_plate, ':');
+			std::getline(fin, all_crimes);
+			if (licence_plate.empty() || all_crimes.empty())continue;
+			all_crimes.erase(all_crimes.find_last_of(';'));
+			for (int start = -1, end = 0; end != std::string::npos; start = end)
+			{
+				start++;	
+				end = all_crimes.find(',', start);
+				std::string place = all_crimes.substr(start, end - start);
+				int id = std::stoi(place, 0, 10);
+				place.erase(0, 1);
+				//place.erase(place.find_last_of(';'));
+				base[licence_plate].push_back(Crime(id, place));
+			}
+		}
 
-    cin.ignore();
-    cout << "Введите начальный номер для вывода: "; getline(cin, str_itlow);
-    itlow = GaiMap.lower_bound(str_itlow);
-    cin.ignore();
-    cout << "Введите конечный номер для вывода: "; getline(cin, str_itupper);
-    itupper = GaiMap.upper_bound(str_itupper);
-
-    cout << "Номер" << "\t\t" << "Нарушения";
-    cout << "\n----------------------------\n";
-
-    for (base_list::iterator car_it = itlow; car_it != itupper; car_it++)
-    {
-        cout << car_it->first;
-        list<std::string> get = car_it->second;
-        for (auto it = get.begin(); it != get.end(); ++it)
-            cout << "\t\t" << (*it) << '\n';
-        cout << endl;
-    }
-
+		fin.close();
+	}
+	else
+	{
+		std::cerr << "Error: File not found" << endl;
+	}
+}
+int check_crime()
+{
+	system("CLS");
+	for (std::pair<int, std::string> i : CRIMES)
+	{
+		cout << i.first << tab << i.second << endl;
+	}
+	int id;
+	cout << "Выбор правонарушения: "; cin >> id;
+	return id;
+}
+std::string input_place()
+{
+	std::string place;
+	cout << "Введите место происшествия:";
+	cin.ignore();
+	std::getline(std::cin, place);
+	return place;
+}
+std::string input_plate()
+{
+	std::string licence_plate;	
+	std::string place;
+	cout << "Введите номер автомобиля: "; cin >> licence_plate;
+	return licence_plate;
+}
+void menu(std::map<std::string, std::list<Crime>>& base, const std::string& filename)
+{
+	char key;
+	do
+	{
+		system("CLS");
+		cout << "1. Вывод всей базы;" << endl;
+		cout << "2. Вывод по номерному знаку;" << endl;
+		cout << "3. Вывод по диапазону номеров;" << endl;
+		cout << "4. Выборка по шаблону;" << endl;
+		cout << "5. Выборка по правонарушению;" << endl;
+		cout << "6. Выборка по адресам;" << endl;
+		cout << "7. Сохранение базы;" << endl;
+		cout << "8. Загрузка базы;" << endl;
+		cout << "9. Добавление записи;" << endl;
+		cout << "Esc - Выход из программы;" << endl;
+		key = _getch();
+		switch (key)
+		{
+		case '1': print(base); break;
+		case '2': 
+		case '3': 
+		case '4': 
+		case '5': 
+		case '6': cout << "Эта опция еще не добавлена. Ожидается в следующем релизе\n";
+			system("PAUSE");
+			break;
+		case '7': save(base, filename); break;
+		case '8': load(base, filename); break;
+		case '9': base[input_plate()].push_back(Crime(check_crime(), input_place())); break;
+		}
+	} while (key != 27);
 }
